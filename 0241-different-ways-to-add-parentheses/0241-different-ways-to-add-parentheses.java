0443210bf3a39ed583439fe8beed3f4c9343f590
Parwinder Singh <103317175+ParwinderBaidwan@users.noreply.github.com>
2024-09-19 13:34:10 +0530

    Time: 1 ms (99.68%), Space: 41.8 MB (84.31%) - LeetHub

--- 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.java
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.java
@@ -1,10 +1,18 @@
 class Solution {
+    // TC : O(N*2^N)
+    // SC : O(N^2 * 2^N) N^2 for dp array
     public List<Integer> diffWaysToCompute(String expression) {
         int n = expression.length() ;
-        return recur(expression, 0, n-1);
+        List<Integer> dp[][] = new ArrayList[n][n];
+        return recur(expression, 0, n-1, dp);
     }
-    public List<Integer> recur(String exp, int start, int end){
+
+    public List<Integer> recur(String exp, int start, int end, List<Integer> dp[][]){
         List<Integer> list = new ArrayList<>();
+
+        if(dp[start][end] != null){
+            return dp[start][end];
+        }
         // base case : single digit
         if(start == end){
             int num = exp.charAt(start) - '0';
@@ -18,13 +26,15 @@ class Solution {
             return list;
         }
         // split
+        // N
         for(int i=start; i <= end; i++){
             if(Character.isDigit(exp.charAt(i))){
                 continue;
             }
             char op = exp.charAt(i);
-            List<Integer> left = recur(exp,start,i-1);
-            List<Integer> right = recur(exp,i+1,end);
+            // 2^N
+            List<Integer> left = recur(exp,start,i-1, dp);
+            List<Integer> right = recur(exp,i+1,end, dp);
             for(int l : left){
                 for(int r : right){
                     if(op == '*'){
@@ -40,6 +50,7 @@ class Solution {
             }
         }
 
+        dp[start][end] = list;
         return list;
     }
 }
