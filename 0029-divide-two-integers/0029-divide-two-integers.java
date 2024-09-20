21b8737499c6f56783808f77b5eb74ecfad178b6
Parwinder Singh <103317175+ParwinderBaidwan@users.noreply.github.com>
2024-09-20 10:57:17 +0530

    Time: 0 ms (100%), Space: 40.8 MB (62.85%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.java
+++ 0029-divide-two-integers/0029-divide-two-integers.java
@@ -2,12 +2,12 @@ class Solution {
     public int divide(int dividend, int divisor) {
         int ans = 0;
         if(divisor==(-1) && dividend<(-1)){
-             ans +=(-1)*(dividend + 1);
+            ans +=(-1)*(dividend + 1);
         }
         else{
-            ans+=dividend/divisor;
-            // int r=dividend%divisor;
-            //  ans +=q-r/divisor;
+            int q=dividend/divisor;
+            int r=dividend%divisor;
+            ans +=q-r/divisor;
         }
         
         return ans;
