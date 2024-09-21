8eced6323322a4fb2d79448f23d5af3c4682e1a9
Parwinder Singh <103317175+ParwinderBaidwan@users.noreply.github.com>
2024-09-21 22:16:55 +0530

    Time: 5 ms (42.61%), Space: 48.4 MB (50.51%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.java
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.java
@@ -1,21 +1,29 @@
 class Solution {
     public List<Integer> lexicalOrder(int n) {
         List<Integer> res = new ArrayList<>();
-        for(int i=1; i<10; i++){
-            if(i>n) break;
-            dfs(i, n, res);
+        int curNum = 1;
+        for(int i=1; i<=n; i++){
+            res.add(curNum);
+            if(curNum*10 <= n){
+                curNum *= 10;
+            } else{
+                while(curNum%10 == 9 || curNum == n){
+                    curNum /= 10;
+                }
+                curNum += 1;
+            }
         }
         return res;
     }
-    public void dfs(int currNum, int target, List<Integer> res){
-        if(currNum > target){
-            return;
-        }
+    // public void dfs(int currNum, int target, List<Integer> res){
+    //     if(currNum > target){
+    //         return;
+    //     }
 
-        res.add(currNum);
-        for(int i=0; i<10; i++){
-            if(currNum*10 + i > target) break;
-            dfs(currNum*10+i, target, res);
-        }
-    }
+    //     res.add(currNum);
+    //     for(int i=0; i<10; i++){
+    //         if(currNum*10 + i > target) break;
+    //         dfs(currNum*10+i, target, res);
+    //     }
+    // }
 }
