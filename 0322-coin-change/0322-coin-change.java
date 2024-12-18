7a3317c8a55183f7214d7ad33c2ac11e167f8332
Parwinder Singh <103317175+ParwinderBaidwan@users.noreply.github.com>
2024-12-18 12:12:46 +0530

    Time: 110 ms (8.02%), Space: 45.3 MB (16.64%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -5,40 +5,34 @@ class Solution {
         // Create a 2D array to store results of subproblems
         int[][] dp = new int[n][amount + 1];
 
-        // Initialize the dp array with -1 to indicate that subproblems are not solved yet
-        for (int row[] : dp)
-            Arrays.fill(row, -1);
+        for(int i=0; i<=amount; i++){
+            if(i % coins[0] == 0){
+                dp[0][i] = i / coins[0];
+            } else{
+                dp[0][i] = (int) Math.pow(10,9);
+            }
+        }
 
-        int ans =  minElements(coins, n-1, amount, dp );
+        for(int ind=1; ind<n; ind++){
+            for(int target=0; target <= amount; target++){
+                int notTake = 0 + dp[ind-1][target];
+                int take = (int) Math.pow(10, 9);
+
+                // If the current element is less than or equal to the target, calculate 'take'
+                if (coins[ind] <= target)
+                    take = 1 + dp[ind][target - coins[ind]];
+
+                // Store the minimum result in the dp array
+                dp[ind][target] = Math.min(notTake, take);
+            }
+        }
+
+        int ans =  dp[n-1][amount];
 
         // If it's not possible to achieve the target sum, return -1
         if (ans >= (int) Math.pow(10, 9))
             return -1;
         return ans;
-    }
 
-    public int minElements(int[] coins, int index, int amount, int[][]dp){
-        //base case
-        if(index == 0){
-            if(amount % coins[0] == 0){
-                return amount / coins[0];
-            } else{
-                return (int) Math.pow(10,9);
-            }
-        }
-
-        // If the result for this subproblem has already been calculated, return it
-        if (dp[index][amount] != -1)
-            return dp[index][amount];
-
-        int notTaken = 0 + minElements(coins, index-1, amount, dp);
-
-        // Initialize the minimum number of elements needed taking the current element
-        int taken = (int)Math.pow(10,9);
-        if(coins[index] <= amount){
-            taken = 1 + minElements(coins, index, amount - coins[index], dp);
-        }
-
-        return dp[index][amount] = Math.min(notTaken, taken);
     }
 }
